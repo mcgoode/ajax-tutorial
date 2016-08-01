8e0843d9344a0762aebdc788b3b07563cc8826c2
chrgoo1 <#darkvirus12>
2016-08-01 16:51:10 -0400

    Initial commit

---
+++ ajax-tutorial.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Ajax Tutorial
+Plugin URI: http://chrisgoode.me/tutorials/ajax-and-wordpress/
+Description: A basic plugin showing how to use ajax.
+Version: 1.0
+Author: Chris Goode
+Author URI: http://chrisgoode.me
+License: GPL2
+*/
+
